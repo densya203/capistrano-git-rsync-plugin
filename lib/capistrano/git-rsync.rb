a397e750ccd32c23fd1707d30fb9d4c55b4c4133
densya203 <densya203@skult.jp>
2023-03-30 16:07:00 +0900

    Update git-rsync.rb
    Variable name refactoring.
    Remove -t option from  rsync options.

--- lib/capistrano/git-rsync.rb
+++ lib/capistrano/git-rsync.rb
@@ -3,20 +3,19 @@ require 'capistrano/scm/plugin'
 class Capistrano::SCM
   class GitRsync < ::Capistrano::SCM::Plugin
     def set_defaults
-       # command-line options for rsync
-      set_if_empty :rsync_options, %w[--archive --delete --exclude=.git*]
+      # command-line options for rsync
+      # Without -t option (timestamp)
+      set_if_empty :rsync_options, %w[-rlpgoD --human-readable --verbose --delete --exclude=.git*]
 
       # Local cache (Git checkout will be happen here, resulting files then get rsynced to the remote server)
-      set_if_empty :rsync_local_cache, "/usr/local/src/capistrano-rsync-deploy/fetch(:application)/#{fetch(:stage)}"
+      set_if_empty :git_checkout_to, "/usr/local/src/capistrano-rsync-deploy/fetch(:application)/#{fetch(:stage)}"
     end
 
     def define_tasks
-      namespace :rsync do
+      namespace :git_rsync do
         desc <<-DESC
             Copy application source code from (remote) cache to release path.
 
-            If a :rsync_deploy_build_path is set, only that relative path will \
-            be copied to the release path.
         DESC
         task create_release: :update_local_cache do
           on release_roles :all do
@@ -28,26 +27,27 @@ class Capistrano::SCM
               discarding_release_path = releases_path.join(can_be_discarded_release_number)
 
               # Set oldest release to this time release target.
-              # Better than starting empty directory, In many cases.
+              # Better than starting from empty directory, In many cases.
               execute :mv, discarding_release_path, release_path
             end
 
-            execute :rsync, '--archive', "#{fetch(:rsync_local_cache)}/", "#{release_path}/"
+            # Without -t option (timestamp)
+            execute :rsync, *fetch(:rsync_options), "#{fetch(:git_checkout_to)}/", "#{release_path}/"
           end
         end
 
         desc <<-DESC
             Update local cache of application source code.
 
-            This will be checked out to :rsync_local_cache.
+            This will be checked out to :git_checkout_to.
         DESC
         task :update_local_cache do
           on release_roles :all do |role|
-            within fetch(:rsync_local_cache) do
-              repo_exists = (capture "if [ -e #{fetch(:rsync_local_cache)}/.git ];then echo yes;fi")
+            within fetch(:git_checkout_to) do
+              repo_exists = (capture "if [ -e #{fetch(:git_checkout_to)}/.git ];then echo yes;fi")
               if repo_exists != 'yes'
-                execute :mkdir, '-p', fetch(:rsync_local_cache)
-                execute :git, :clone, '--quiet', repo_url, fetch(:rsync_local_cache)
+                execute :mkdir, '-p', fetch(:git_checkout_to)
+                execute :git, :clone, '--quiet', repo_url, fetch(:git_checkout_to)
               end
 
               execute :git, :fetch, '--quiet', '--all', '--prune'
@@ -64,7 +64,7 @@ class Capistrano::SCM
         DESC
         task :set_current_revision do
           on release_roles :all do |role|
-            within fetch(:rsync_local_cache) do
+            within fetch(:git_checkout_to) do
               set :current_revision, capture(:git, "rev-list --max-count=1 #{fetch(:branch)}")
             end
           end
@@ -73,8 +73,8 @@ class Capistrano::SCM
     end
 
     def register_hooks
-      after 'deploy:new_release_path', 'rsync:create_release'
-      before 'deploy:set_current_revision', 'rsync:set_current_revision'
+      after  'deploy:new_release_path'    , 'git_rsync:create_release'
+      before 'deploy:set_current_revision', 'git_rsync:set_current_revision'
     end
   end
 end
